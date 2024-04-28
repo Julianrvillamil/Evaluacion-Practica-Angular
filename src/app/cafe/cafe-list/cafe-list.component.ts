3ea8d6b4fb743cf973019d921f97fb2278a32433
julianrvillamil <julianrvillamil@gmail.com>
2024-04-27 19:57:12 -0500

    Punto No 2 Listo

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -9,16 +9,30 @@ import { CafeService } from '../cafe.service';
 })
 export class CafeListComponent implements OnInit {
   cafes: Array<Cafe> = [];
+  cafesPorTipo: { [tipo: string]: number } = {};
 
   constructor(private cafeService: CafeService) {}
 
   getCafes(): void {
     this.cafeService.getCafes().subscribe((cafes) => {
       this.cafes = cafes;
+      this.calcularVariedadesPorTipo();
     });
   }
 
   ngOnInit(): void {
     this.getCafes();
+  }
+
+  private calcularVariedadesPorTipo(): void {
+    this.cafesPorTipo = {};
+    this.cafes.forEach((cafe) => {
+      if (this.cafesPorTipo[cafe.tipo]) {
+        this.cafesPorTipo[cafe.tipo]++;
+      } else {
+        this.cafesPorTipo[cafe.tipo] = 1;
+      }
+    });
+    console.log(this.cafesPorTipo);
   }
 }
